f84b1291382bbda70a10110fda6cc9986d5f012e
Sunaina <81823428+sunaina09@users.noreply.github.com>
2021-08-02 18:08:53 +0530

    Add files via upload

---
+++ StaticBlock.java
@@ -0,0 +1,12 @@
+//Nested class
+
+class StaticBlock{
+
+ static{
+     System.out.println("Static block is invoked");  
+ }
+
+  public static void main(String []args){
+     System.out.println("Hello main");  
+  }
+}
